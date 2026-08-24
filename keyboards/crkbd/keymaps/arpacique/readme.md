e5196a51db26bf0c8dc32e33c5c37698fc83c6d4
arpacique <kemalemrearpaci@gmail.com>
2026-08-24 13:19:39 +0300

    add corne

---
+++ keyboards/crkbd/keymaps/arpacique/readme.md
@@ -0,0 +1,4 @@
+# @arpacique keymap for Corne (crkbd/rev1)
+
+This is the Reviung41 layout adapted to the Corne's six-key thumb cluster.
+Both inner thumb keys tap for Space and hold the Arrow layer.
--- keyboards/reviung41/keymaps/arpacique/keymap.c
+++ keyboards/reviung41/keymaps/arpacique/keymap.c
@@ -28,6 +28,18 @@ enum layer_names {
 #define ARROW  MO(_ARROW)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+  /* _BASE (Layer 0) - QWERTY with Modifiers
+   * ┌───┬───┬───┬───┬───┬───┐           ┌───┬───┬───┬───┬───┬───┐
+   * │Tab│ Q │ W │ E │ R │ T │           │ Y │ U │ I │ O │ P │Bsp│
+   * ├───┼───┼───┼───┼───┼───┤           ├───┼───┼───┼───┼───┼───┤
+   * │Esc│A◇ │S⌥ │D⇧ │F⌃ │ G │           │ H │J⌃ │K⇧ │L⌥ │;◇ │ ' │
+   * ├───┼───┼───┼───┼───┼───┤           ├───┼───┼───┼───┼───┼───┤
+   * │⇧  │ Z │ X │ C │ V │ B │           │ N │ M │ , │ . │ / │⇧↵ │
+   * └───┴───┴───┴───┴───┴───┘           └───┴───┴───┴───┴───┴───┘
+   *     ┌────┬────┬──────────┬────┬────┐
+   *     │ ⌃  │Low │Spc(Arr)  │Rse │Gui │
+   *     └────┴────┴──────────┴────┴────┘
+   */
   [_BASE] = LAYOUT_reviung41(
   KC_TAB,     KC_Q,        KC_W,        KC_E,       KC_R,         KC_T,                    KC_Y,  KC_U,        KC_I,        KC_O,        KC_P,           KC_BSPC,
   KC_ESC,     GUI_T(KC_A), ALT_T(KC_S), SFT_T(KC_D), CTL_T(KC_F), KC_G,                    KC_H,  CTL_T(KC_J), SFT_T(KC_K), ALT_T(KC_L), GUI_T(KC_SCLN), KC_QUOT,
@@ -35,6 +47,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                    KC_LCTL,   LOWER,    LT(ARROW, KC_SPC),   RAISE,    KC_RGUI
   ),
   
+  /* _LOWER (Layer 1) - Numbers & Symbols
+   * ┌───┬───┬───┬───┬───┬───┐           ┌───┬───┬───┬───┬───┬───┐
+   * │ ~ │ 1 │ 2 │ 3 │ 4 │ 5 │           │ ^ │ & │ * │ ( │ ) │Del│
+   * ├───┼───┼───┼───┼───┼───┤           ├───┼───┼───┼───┼───┼───┤
+   * │Del│ F1│ F2│ F3│ F4│ F5│           │ F6│ _ │ + │ { │ } │ | │
+   * ├───┼───┼───┼───┼───┼───┤           ├───┼───┼───┼───┼───┼───┤
+   * │Cap│ F7│ F8│ F9│F10│F11│           │F12│   │   │Hom│End│   │
+   * └───┴───┴───┴───┴───┴───┘           └───┴───┴───┴───┴───┴───┘
+   *     ┌────┬────┬──────────┬────┬────┐
+   *     │    │    │          │    │    │
+   *     └────┴────┴──────────┴────┴────┘
+   */
   [_LOWER] = LAYOUT_reviung41(
     KC_TILD, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,             KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL,
     KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,            KC_F6,   KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE,
@@ -42,6 +66,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                       _______,   _______,  _______,   _______,  _______
   ),
   
+  /* _RAISE (Layer 2) - Numbers & Symbols (Alt layout)
+   * ┌───┬───┬───┬───┬───┬───┐           ┌───┬───┬───┬───┬───┬───┐
+   * │ ` │ ! │ @ │ # │ $ │ % │           │ 6 │ 7 │ 8 │ 9 │ 0 │Del│
+   * ├───┼───┼───┼───┼───┼───┤           ├───┼───┼───┼───┼───┼───┤
+   * │Del│ F1│ F2│ F3│ F4│ F5│           │ F6│ - │ = │ [ │ ] │ \ │
+   * ├───┼───┼───┼───┼───┼───┤           ├───┼───┼───┼───┼───┼───┤
+   * │Cap│ F7│ F8│ F9│F10│F11│           │F12│ < │ > │PgU│PgD│   │
+   * └───┴───┴───┴───┴───┴───┘           └───┴───┴───┴───┴───┴───┘
+   *     ┌────┬────┬──────────┬────┬────┐
+   *     │    │    │          │    │    │
+   *     └────┴────┴──────────┴────┴────┘
+   */
   [_RAISE] = LAYOUT_reviung41(
   KC_GRV,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,              KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,
   KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,
@@ -49,6 +85,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                       _______,   _______,  _______,  _______,  _______
   ),
   
+  /* _ADJUST (Layer 3) - RGB Lighting & Settings
+   * ┌────┬────┬────┬────┬────┬────┐     ┌────┬────┬────┬────┬────┬────┐
+   * │UGV+│UGS+│UGH+│UGNx│    │UG𝐓g│     │    │    │    │    │    │RBT │
+   * ├────┼────┼────┼────┼────┼────┤     ├────┼────┼────┼────┼────┼────┤
+   * │UGV-│UGS-│UGH-│UGPr│    │CG𝐍m│     │CG𝐒w│    │    │    │    │    │
+   * ├────┼────┼────┼────┼────┼────┤     ├────┼────┼────┼────┼────┼────┤
+   * │    │    │    │    │    │    │     │    │    │    │    │    │    │
+   * └────┴────┴────┴────┴────┴────┘     └────┴────┴────┴────┴────┴────┘
+   *     ┌────┬────┬──────────┬────┬────┐
+   *     │    │    │          │    │    │
+   *     └────┴────┴──────────┴────┴────┘
+   */
   [_ADJUST] = LAYOUT_reviung41(
     UG_VALU,   UG_SATU, UG_HUEU,  UG_NEXT,  XXXXXXX,   UG_TOGG,            XXXXXXX,   XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  QK_REBOOT,
     UG_VALD,   UG_SATD, UG_HUED,  UG_PREV,  XXXXXXX,   CG_NORM,            CG_SWAP,   XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
@@ -56,6 +104,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                             _______,   _______,  XXXXXXX,  _______,  _______
   ),
   
+  /* _ARROW (Layer 4) - Navigation & Numpad
+   * ┌───┬───┬───┬───┬───┬───┐           ┌───┬───┬───┬───┬───┬───┐
+   * │   │   │   │ ↑ │C-H│PgU│           │   │ 7 │ 8 │ 9 │ / │Bsp│
+   * ├───┼───┼───┼───┼───┼───┤           ├───┼───┼───┼───┼───┼───┤
+   * │   │Hom│ ← │ ↓ │ → │End│           │   │ 4 │ 5 │ 6 │ * │ = │
+   * ├───┼───┼───┼───┼───┼───┤           ├───┼───┼───┼───┼───┼───┤
+   * │   │   │   │   │C-E│PgD│           │ 0 │ 1 │ 2 │ 3 │ - │ + │
+   * └───┴───┴───┴───┴───┴───┘           └───┴───┴───┴───┴───┴───┘
+   *     ┌────┬────┬──────────┬────┬────┐
+   *     │    │    │          │ 0  │ .  │
+   *     └────┴────┴──────────┴────┴────┘
+   */
   [_ARROW] = LAYOUT_reviung41(
     _______, _______, _______, KC_UP  , C(KC_HOME), KC_PGUP,          _______, KC_7, KC_8, KC_9,  KC_SLASH,    KC_BSPC,
     _______, KC_HOME, KC_LEFT, KC_DOWN, KC_RGHT,    KC_END,           _______, KC_4, KC_5, KC_6,  KC_ASTERISK, KC_EQUAL,
